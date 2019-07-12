cdbcab15de707880a0e0189bcefc8bd95e7d807b
Cloud User <ec2-user@ip-172-31-36-217.ap-south-1.compute.internal>
2019-07-12 07:44:52 +0000

    palindrome

--- pallindrom.c
+++ pallindrom.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
  
-int main()
+void palindrome()
 {
    int n, reverse = 0, t;
  
@@ -21,7 +21,6 @@ int main()
    else
       printf("%d isn't a palindrome number.\n", n);
  
-   return 0;
 }
 
 
